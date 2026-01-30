eefbd96348de77abbe8e82edf1fd12625186e5b5
IsmaelSantiagoo <ismaelfreitas.santiago@gmail.com>
2026-01-29 21:51:03 -0300

    remover middleware desnecessário

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,6 @@
 
 use App\Http\Controllers\Auth\RegisterTenantController;
 use App\Http\Controllers\Auth\AuthController;
-use App\Http\Controllers\TenantController;
 use Illuminate\Support\Facades\Route;
 use App\Support\AppRouter;
 use Illuminate\Support\Facades\Broadcast;
@@ -13,7 +12,7 @@ Route::post('auth/login', [AuthController::class, 'login']);
 Route::post('auth/register', [AuthController::class, 'register']);
 Route::post('/register-tenant', [RegisterTenantController::class, 'store']);
 
-Route::middleware(['api', 'auth:sanctum', 'identify.tenant'])->group(function () {
+Route::middleware(['auth:sanctum', 'identify.tenant'])->group(function () {
     // protegidas
     Route::get('auth/me', [AuthController::class, 'me']);
     Route::get('auth/logout', [AuthController::class, 'logout']);
