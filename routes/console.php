5738e8e4dae9ec893c34f5f0fca3ed5f1410f008
Ismael Santiago <87278894+IsmaelSantiagoo@users.noreply.github.com>
2026-06-22 10:50:47 -0300

    feat: add daily scheduled task to clear uploads in console routes (#7)

--- routes/console.php
+++ routes/console.php
@@ -6,3 +6,8 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Schedule::command('uploads:clear')
+    ->dailyAt('03:00')       // Força a rodar SÓ às 3 da manhã
+    ->runInBackground()      // Libera a CPU imediatamente
+    ->withoutOverlapping();  // Impede que acumule processos se travar
